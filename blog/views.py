711ad09a42a779fad8c37519cc846140719db382
André P <andrepollux@icloud.com>
2022-10-01 16:47:32 +0000

    Finish database optimization

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
@@ -41,3 +41,8 @@ def post_detail(request, slug):
     return render(
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
     )
+
+
+def get_ip(request):
+    from django.http import HttpResponse
+    return HttpResponse(request.META['REMOTE_ADDR'])
