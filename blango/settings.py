cb9b2892502bd7f98f429b733843b2ab5bb70d99
André P <andrepollux@icloud.com>
2022-10-05 18:25:24 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -49,6 +49,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'blango_auth',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
@@ -175,6 +176,8 @@ class Dev(Configuration):
     }
 
     INTERNAL_IPS = ["192.168.10.226"]
+
+    AUTH_USER_MODEL = "blango_auth.User"
 
 
 class Prod(Dev):
